e6cef5bd993d26f19ff52b1331cbbf9ef70bbef9
Matheus Henrique Rodrigues <matheushenri4141@gmail.com>
2025-02-02 16:59:08 -0300

    Inclusão dos diagramas e decisoes de projeto

--- CadastroDeClientes/CadastroDeClientes.API/ViewModels/UsuarioViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CadastroDeClientes.API.ViewModels
-{
-    public class UsuarioViewModel
-    {
-        public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public string Role { get; private set; }
-    }
-}
